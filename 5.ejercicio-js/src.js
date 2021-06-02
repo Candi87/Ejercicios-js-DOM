f3da964a152d9a2fc391dfe4eaf46b9057c56b2d
Candi87 <kndy_1987@hotmail.com>
2021-06-02 09:50:51 +0200

    ultimos retoques

--- 5.ejercicio-js/src.js
+++ 5.ejercicio-js/src.js
@@ -4,32 +4,22 @@
 
 // Utiliza llamadas a la API: 'https://rickandmortyapi.com/api/'
 
-'use strict';
-let air_date;
-let name;
-async function getCharacters(air_date = '') {
-    const URL = `https://rickandmortyapi.com/api/episode/?air_date=${air_date}`;
+const API_URL = 'https://rickandmortyapi.com/api/character/';
 
+const getCharacters = async () => {
     try {
-        const response = await fetch(URL);
-        const data = await response.json();
-
-        const air_date = [...data.results];
-        let nextPage = data.info.next;
-
-        while (nextPage !== null) {
-            const response = await fetch(nextPage);
-            const data = await response.json();
-
-            air_date.push(...data.results);
-
-            nextPage = data.info.next;
-        }
-
-        console.log(air_date);
-    } catch (error) {
-        console.log(error);
+        const result = await fetch(API_URL).then((result) => result.json());
+        return result;
+    } catch (e) {
+        console.error(e);
+        return undefined;
     }
-}
+};
 
-getCharacters('January');
+(async () => {
+    const nombresAPI = await getCharacters();
+    if (nombresAPI) {
+        const nombres = nombresAPI.results.map((element) => element.name);
+        console.log(nombres);
+    }
+})();
--- 6.ejercicio-dom/ejercicio.js
+++ 6.ejercicio-dom/ejercicio.js
@@ -1,1 +1,12 @@
 'use strict';
+const parrafo = document.querySelectorAll('p').item(0);
+let parrafoHtml = parrafo.innerHTML;
+let parrafoSub = parrafoHtml.split(/[\s,\.,\"]+/);
+
+parrafoSub.forEach((palabra) => {
+    if (palabra.length > 5) {
+        parrafoHtml = parrafoHtml.split(palabra).join(`<u>${palabra}</u>`);
+    }
+});
+
+parrafo.innerHTML = parrafoHtml;
